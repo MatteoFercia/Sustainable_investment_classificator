009bdfcc11c07272ba435d4ceb8f85fd2e6aeed8
Matteo_Fercia <79990772+MatteoFercia@users.noreply.github.com>
2022-04-26 23:29:44 +0200

    Add files via upload

---
+++ lista_paesi.py
@@ -0,0 +1,47 @@
+
+'''
+Nel seguente file abbiamo inserito la lista contenente altre sottoliste in cui troviamo i 37 paesi scelti
+per la nostra analisi e le coordinate delle loro capitali:
+il primo elemento della lista serve per specificare allo scraper la geolocalizzazione dei tweets,
+mentre il secondo elemento serve per indicare il nome del file.csv in cui i tweets relativi andranno inseriti.
+'''
+
+country_list = [
+    ['37.8136, 144.9631, 1000km', 'Australia'],
+    ['48.2082, 16.3738, 1000km', 'Austria'],
+    ['50.8503, 4.3517, 1000km', 'Belgium'],
+    ['43.6532, 79.3832, 1000km', 'Canada'],
+    ['33.4489, 70.6693, 1000km', 'Chile'],
+    ['4.7110, 74.0721, 1000km', 'Colombia'],
+    ['9.9281, 84.0907, 1000km', 'Costa Rica'],
+    ['50.0755, 14.4378, 1000km', 'Czech Republic'],
+    ['55.6761, 12.5683, 1000km', 'Denmark'],
+    ['59.4370, 24.7536, 1000km', 'Estonia'],
+    ['60.1699, 24.9384, 1000km', 'Finland'],
+    ['48.8566, 2.3522, 1000km', 'France'],
+    ['52.5200, 13.4050, 1000km', 'Germany'],
+    ['37.9838, 23.7275, 1000km', 'Greece'],
+    ['47.4979, 19.0402, 1000km', 'Hungary'],
+    ['64.1466, 21.9426, 1000km', 'Iceland'],
+    ['53.3498, 6.2603, 1000km', 'Ireland'],
+    ['32.0853, 34.7818, 1000km', 'Israel'],
+    ['41.9028, 12.4964, 1000km', 'Italy'],
+    ['35.4123, 139.4132, 1000km', 'Japan'],
+    ['37.33, 126.59, 1000km', 'South Korea'],
+    ['54.687157, 25.279652, 1000km', 'Lithuania'],
+    ['56.946285, 24.105078, 1000km', 'Latvia'],
+    ['49.3642, 6.748, 1000km', 'Luxembourg'],
+    ['19.2602, 99.080156, 1000km', 'Mexico'],
+    ['52.377956, 4.897070, 1000km', 'Netherlands'],
+    ['-41.2867, 174.7730, 1000km', 'New Zealand'],
+    ['59.5440, 10.4510, 1000km', 'Norway'],
+    ['38.42, 9.11, 1000km', 'Portugal'],
+    ['48.148598, 17.107748, 1000km', 'Slovakia'],
+    ['46.056946, 14.505751, 1000km', 'Slovenia'],
+    ['40.4168, 3.7038, 1000km', 'Spain'],
+    ['59.3293, 18.0686, 1000km', 'Sweden'],
+    ['46.2044, 6.1432, 1000km', 'Switzerland'],
+    ['46.2044, 6.1432, 1000km', 'Turkey'],
+    ['51.5074, 0.1278, 1000km', 'United Kingdom'],
+    ['38.9072, 77.0369, 1000km', 'United States'],
+]
---
+++ scraper_twitter.py
@@ -0,0 +1,103 @@
+import datetime
+import os
+import sys
+from lista_paesi import country_list
+from nltk import re
+from textblob import TextBlob
+import snscrape.modules.twitter as sntwitter
+import csv
+
+
+
+def clean_text(tweet):
+    '''
+    Regular expression that removes links and special characters from tweet.
+    '''
+    return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(https?\S+)", " ", tweet).split())
+
+
+def get_tweet_sentiment(tweet):
+    '''
+    Calculate the sentiment using TextBlob module
+    TextBlob
+    '''
+    text = clean_text(tweet)
+    analysis = TextBlob(text)
+    if analysis.sentiment.polarity > 0:
+        return 'positive'
+    elif analysis.sentiment.polarity == 0:
+        return 'neutral'
+    else:
+        return 'negative'
+
+
+def get_tweets_sentiment(query, count=2500, start=None, end=None, lang='english'):
+    '''
+    Given a query returns the tweets
+    '''
+    tweets = []
+    try:
+        # calls the API to obtain tweets
+        fetched_tweets = get_tweets(
+            query,
+            count=count,
+            start=start,
+            end=end,
+            lang=lang,
+        )
+        # parsing the tweets
+        for tweet in fetched_tweets:
+            parsed_tweet = {}
+            # get the tweet text
+            parsed_tweet['text'] = tweet
+            # get the sentiment for the tweet's text
+            parsed_tweet['sentiment'] = get_tweet_sentiment(tweet)
+            tweets.append(parsed_tweet)
+        return tweets
+    except Exception as e:
+        exc_type, exc_obj, exc_tb = sys.exc_info()
+        fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+        print(str(e), fname, exc_tb.tb_lineno)
+        return tweets
+
+
+def get_tweets(query, count=2500, start=None, end=None, lang='english'):
+    '''
+    Restituisce i tweet data una query particolare
+    '''
+    if not start:
+        end = datetime.date.today()
+        start = (end - datetime.timedelta(days=30))
+    tweets = []
+    query = f'{query} since:{start} until:{end}'
+    try:
+        # calls the API to obtain tweets
+        fetched_tweets = sntwitter.TwitterSearchScraper(query).get_items()
+        # parsing the tweets
+        for i, tweet in enumerate(fetched_tweets):
+            if i > count:
+                return tweets
+            if tweet.content:
+                # add the tweet to our list and avoid retweets
+                if tweet.retweetCount > 0:
+                    if tweet.content not in tweets:
+                        tweets.append(tweet.content)
+                else:
+                    tweets.append(tweet.content)
+        return tweets
+    except Exception as e:
+        exc_type, exc_obj, exc_tb = sys.exc_info()
+        fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+        print(str(e), fname, exc_tb.tb_lineno)
+        return tweets
+
+
+if __name__ == '__main__':
+    for item in country_list:
+        query = 'green energy geocode:"{}"'.format(item[0])
+        count = 2500
+        with open('{}.csv'.format(item[1]), 'a') as csvfile:
+            for tweet in get_tweets_sentiment(query, count):
+                writer = csv.writer(csvfile)
+                writer.writerow((tweet['sentiment'], tweet['text']))
+
